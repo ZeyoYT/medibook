67ddb2d70c621f4a60dc1079a37587db594f457c
Hetosk1 <hetekta2709@gmail.com>
2024-08-31 01:13:54 +0530

    Signin Completed

---
+++ src/app/api/user/decode/route.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+import { NextResponse } from 'next/server';
+
+export async function POST(_request){
+    const body = await _request.json();
+    
+    const decoded = jwt.verify(body.token, "lundmaro");
+
+    return NextResponse.json({
+        decoded
+    });
+
+
+}
--- src/app/api/user/signin/route.js
+++ src/app/api/user/signin/route.js
@@ -1,8 +1,67 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import {Surreal} from 'surrealdb.js';
+import {connect} from '../../db';
 
-export async function POST(){
-    return NextResponse.json({
-        message: "Welcome to Route: http://localhost:3000/api/user/signin",
-        method: "POST"
-    });
+import jwt from 'jsonwebtoken'
+
+
+
+
+async function areCredentialsTrue(_data){
+
+    const db = await connect();
+
+    const contact = _data.contact;
+    const password = _data.password;
+
+    const user = await db.query(`
+        SELECT * FROM user 
+        WHERE 
+        contact="${contact}"
+        AND
+        password="${password}";
+    `);
+
+
+    if(user){
+        return [true, {user}];
+    } else {
+        return false;
+    }
+
+}
+
+export async function POST(_request){
+ 
+    const body = await _request.json();
+    const user = await areCredentialsTrue(body);
+
+    console.log('avv-loda') ;
+    const filteredUser = user[1].user[0][0]; 
+    
+
+
+    if(filteredUser){
+
+        const token = await jwt.sign(
+            filteredUser, "lundmaro"
+        );
+
+        return NextResponse.json({
+            message: "Credentials are true",
+            token: token,
+            user
+        });
+
+    } else {
+
+        return NextResponse.json({
+            message: "Credentials Invalid",
+            status: 403
+        });
+
+    }
+
+
+
 }
